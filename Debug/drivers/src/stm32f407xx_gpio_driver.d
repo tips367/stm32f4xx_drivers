d10ee7ffe4c6c5597fbac985e42502cd4fbbc675
tips367 <tipu367.khan@hotmail.com>
2021-04-03 04:04:23 +0530

    deleting out files

--- Debug/drivers/src/stm32f407xx_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/src/stm32f407xx_gpio_driver.o: \
- ../drivers/src/stm32f407xx_gpio_driver.c \
- I:/STUDY/Projects/Embedded/SW_forSTM32/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
- I:/STUDY/Projects/Embedded/SW_forSTM32/stm32f4xx_drivers/drivers/inc/stm32f407xx.h
-
-I:/STUDY/Projects/Embedded/SW_forSTM32/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
-
-I:/STUDY/Projects/Embedded/SW_forSTM32/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
--- Debug/src/main.d
+++
@@ -1,4 +0,0 @@
-src/main.o: ../src/main.c \
- I:/STUDY/Projects/Embedded/SW_forSTM32/stm32f4xx_drivers/drivers/inc/stm32f407xx.h
-
-I:/STUDY/Projects/Embedded/SW_forSTM32/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
