f6a58264457472f3d7e79849520ae962bd0e01a2
tips367 <tipu367.khan@hotmail.com>
2021-06-30 17:40:05 +0530

    I2C handle and configuration structure

--- drivers/inc/stm32f407xx_i2c_driver.h
+++ drivers/inc/stm32f407xx_i2c_driver.h
@@ -8,6 +8,45 @@
 #ifndef INC_STM32F407XX_I2C_DRIVER_H_
 #define INC_STM32F407XX_I2C_DRIVER_H_
 
+#include "stm32f407xx.h"
 
+/*
+ * Configuration structure for I2Cx peripheral
+ */
+typedef struct
+{
+	uint32_t I2C_SCLSpeed;
+	uint8_t  I2C_DeviceAddress;
+	uint8_t  I2C_AckControl;
+	uint8_t  I2C_FMDutyCycle;
+}I2C_Config_t;
+
+/*
+ * Handle structure for I2Cx peripheral
+ */
+typedef struct
+{
+	I2C_RegDef_t 	*pI2Cx;
+	I2C_Config_t 	I2C_Config;
+}I2C_Handle_t;
+
+/*
+ * @I2C_SCLSpeed
+ */
+#define I2C_SCL_SPEED_STDMODE 		100000
+#define I2C_SCL_SPEED_FASTMODE4K 	400000
+#define I2C_SCL_SPEED_FASTMODE2K  	200000
+
+/*
+ * @I2C_AckControl
+ */
+#define I2C_ACK_ENABLE        1
+#define I2C_ACK_DISABLE       0
+
+/*
+ * @I2C_FMDutyCycle
+ */
+#define I2C_FM_DUTY_2        0
+#define I2C_FM_DUTY_16_9     1
 
 #endif /* INC_STM32F407XX_I2C_DRIVER_H_ */
